0c101f0e103e0667eb047037a363018f6afeee57
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-07-23 00:12:46 +0100

    install packages + update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PdfParserService } from './pdf-parser/pdf-parser.service';
 import { DrizzleModule } from './drizzle/drizzle.module';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [DrizzleModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    DrizzleModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    HttpModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PdfParserService],
 })
 export class AppModule {}
