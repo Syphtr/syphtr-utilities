856af1c897621ba10562c10b42ede2db424812f3
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-07-23 00:12:07 +0100

    add test route for file uploading

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { PdfParserService } from './pdf-parser/pdf-parser.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private pdfService: PdfParserService,
+  ) {}
 
   @Get()
   getProfiles() {
     return this.appService.getProfile();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadPdf(@UploadedFile() file: Express.Multer.File): Promise<any> {
+    const text = await this.pdfService.processPdf(file);
+    // const transformedText = await this.pdfService.generateJsonData(text);
+
+    return text;
   }
 }
